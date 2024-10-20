62e05a340ea5a84622497d9f35d0e6f28a86c3e3
stasel <2033301+stasel@users.noreply.github.com>
2024-10-20 12:52:07 +0200

    Added a nice background to the welcome page

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -6,6 +6,7 @@ import { START_QUIZ_BUTTON_ID } from '../constants.js';
  */
 export const createWelcomeElement = () => {
   const element = document.createElement('div');
+  element.style.backgroundColor = 'lightblue';
   element.innerHTML = String.raw`
     <h1>Welcome</h1>
     <button id="${START_QUIZ_BUTTON_ID}">start quiz</button>
